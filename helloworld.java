9e10fe86253fbd40dd9f6f343b0b5abf8aee86f4
enctsober <bobby0209@163.com>
2020-08-22 18:00:49 +0800

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+ System.out.println("helloworld");
+ System.out.println("1");
+System.out.println("helloworld");
+ System.out.println("2");
+    }
+}
